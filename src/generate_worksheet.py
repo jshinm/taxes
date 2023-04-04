816d42246d6dd6a6b43e8952ceb819b042468865
Jong Shin <jshin.m@gmail.com>
2023-04-03 23:42:26 -0400

    reformat dates and simplify code

--- src/generate_worksheet.py
+++ src/generate_worksheet.py
@@ -31,26 +31,39 @@ class generate_worksheet(RW):
             self.df_all_labeled[i] = tmp_df
 
     def combine_worksheet(self, to_file=False):
-        self.df_concat = self.df_all_labeled[0]
-        self.df_concat = pd.concat([self.df_concat, self.df_all_labeled[1]])
-        self.df_concat = pd.concat([self.df_concat, self.df_all_labeled[2]])
+        def format_dates(x):
+            pattern = r"[0-9]{4}-[0-9]{2}-[0-9]{2}"
+            return re.findall(pattern, str(x))[0]
+        
+        self.df_concat = None
+        
+        for df in self.df_all_labeled:
+            try:
+                self.df_concat = pd.concat([self.df_concat, df])
+            except:
+                self.df_concat = df
         
         self.oos_worksheet()
         self.df_concat = pd.concat([self.df_concat, self.df_oos])
 
-        self.df_concat.sort_values(['Category'], inplace=True)
-        # self.df_concat['Posted Date'] = self.df_concat['Posted Date'].apply(lambda x: x.date())
+        self.df_concat.drop_duplicates(inplace=True)
+        
+        self.df_concat.Date = self.df_concat.Date.apply(lambda x: format_dates(x))
+        self.df_concat.sort_values(['Category', 'Date'], inplace=True)
 
         if to_file:
-            self.df_concat.to_excel(f'{self.year}_combined_loss.xlsx', index=False)
+            folderPath = os.path.join('tax', self.year, 'output')
+            self.df_concat.to_excel(f'{folderPath}/{self.year}_combined_loss.xlsx', index=False)
 
     def categorical_worksheet(self, to_file=False):
+        folderPath = os.path.join('tax', self.year, 'output')
+        
         self.df_categorical = self.df_concat.groupby('Category')[['Amount']].sum();
         self.df_categorical.Amount = abs(self.df_categorical.Amount)
         self.df_categorical.sort_index(inplace=True)
 
         if to_file:
-            self.df_categorical.to_excel(f'{self.year}_categorical_loss.xlsx')
+            self.df_categorical.to_excel(f'{folderPath}/{self.year}_categorical_loss.xlsx')
 
     def oos_worksheet(self):
         _folder_path = os.path.join('tax', self.year, 'out-of-scope')
