1b004dfd09533f560da5c318e2735bde0a839c9a
Jong Shin <jshin.m@gmail.com>
2023-04-03 23:44:44 -0400

    add more accounts

--- src/read_worksheet.py
+++ src/read_worksheet.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import os
 import warnings
 
 from pandas.core.common import SettingWithCopyWarning
@@ -29,9 +30,11 @@ class read_worksheet(LC):
         self.df_cc0 = self.df_cc0[label1]
         self.df_cc1 = self.df_cc1[label1]
         self.df_mr = self.df_mr[label1]
-        self.df_amex = self.df_cc1[label1]
+        self.df_amex = self.df_amex[label1]
 
-        self.df_all = [self.df_check, self.df_cc0, self.df_cc1, self.df_mr, self.df_amex]
+        self.df_all = [self.df_check, self.df_cc0, \
+                       self.df_cc1, self.df_mr, \
+                       self.df_amex]
 
         self.criteria = self.criteria
 
@@ -43,13 +46,14 @@ class read_worksheet(LC):
     def preprocess(self, df_num=0, filter=None, as_file=False):
         self.sheet_name = ['check', 'cc0', 'cc1', 'mr', 'amex'][df_num]
         df = self.df_all[df_num]
+        df['Account'] = self.sheet_name
 
         self.profit = df.query('Amount > 0')
         self.loss = df.query('Amount < 0')
 
         if as_file:
-            self.profit.to_excel(f'profit_{self.sheet_name}.xlsx', index=False)
-            self.loss.to_excel(f'loss_{self.sheet_name}.xlsx', index=False)
+            self.profit.to_excel(f'tax/{self.year}/profit/profit_{self.sheet_name}.xlsx', index=False)
+            self.loss.to_excel(f'tax/{self.year}/loss/loss_{self.sheet_name}.xlsx', index=False)
 
     def exclude_all(self, include_retail=True, to_file=False):
         tmp = []
@@ -60,17 +64,22 @@ class read_worksheet(LC):
             if include_retail:
                 self.read_costco()
                 self.read_amazon()
+                
                 tmp += self.costco
                 tmp += self.amazon
+                
                 self._misc_save(category=self.costco, fname=f'loss_{self.sheet_name}_costco.xlsx')
                 self._misc_save(category=self.amazon, fname=f'loss_{self.sheet_name}_amazon.xlsx')
 
-            self.loss[~self.loss.Description.isin(tmp)].to_excel(f'loss_{self.sheet_name}_exclude_all.xlsx', index=False)
+            folderPath = os.path.join('tax', self.year, 'uncategorized')
+            filePath = f'loss_{self.sheet_name}_exclude_all.xlsx'
+            self.loss[~self.loss.Description.isin(tmp)].to_excel(os.path.join(folderPath, filePath), index=False)
 
     def _misc_save(self, category, fname):
+        folderPath = os.path.join('tax', self.year, 'out-of-scope')
         tmp_df = self.loss[self.loss.Description.isin(category)]
-        tmp_df['Category'] = 'miscellaneous'
-        tmp_df.to_excel(fname, index=False)
+        tmp_df['Category'] = 'operation'
+        tmp_df.to_excel(os.path.join(folderPath,fname), index=False)
 
     def read_costco(self):
         self.costco = [i for i in self.loss.Description if re.findall(pattern='costco', string=str(i).lower())]
